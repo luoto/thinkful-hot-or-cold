155d3678e32b4cb4e68b2d67895932a789886369
Tony Luo <luo.tony37@gmail.com>
2015-04-10 20:25:59 +0900

    added most of the basic functionality, need option to provide user with hot/cold feedback

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
+var guess;
+var targetNum;
+var numGuesses;
+var guesses;
 
 $(document).ready(function(){
-	
+    newGame();
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -12,6 +17,59 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+    $(".new").click(function() {
+        newGame();
+    });
+
+    $("#guessButton").click(function(e) {
+        e.preventDefault();
+
+        //get and validate user input
+        guess = $('#userGuess').val();
+        if(!validInput(guess)) {
+            updateFeedbackMsg('Guess must be between 0 and 100');
+            return;
+        }
+
+        if(guess == targetNum) {
+            updateFeedbackMsg('1000% HOT, Congrats!');
+            $('#guessButton').prop('disabled', true);
+        }
+
+        $('#count').text(++numGuesses);
+        $('#guessList').append('<li>' + guess + '</li>');
+    });
+
 });
 
+/*--- Starts a new game by resetting the fields ---*/
+function newGame() {
+    console.log('new game started');
+    numGuesses = 0;
+    targetNum = randNum();
+    $('#guessList').html('');
+    $('#feedback').text('Make your Guess!');
+    $('#count').text('0');
+    $('#guessButton').prop('disabled', false);
+};
 
+function randNum() {
+    return Math.floor(Math.random() * 100 + 1);
+};
+
+function validInput(guess) {
+
+    if(isNaN(+guess)) {
+        return false;
+    }
+    else if(guess < 0 || guess > 100) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+function updateFeedbackMsg (msg) {
+    $('#feedback').text(msg);
+}
